deed1c9b24eadd4feab60d8d30ab9f63d785b90f
Michael Orji <orjimichael4886@gmail.com>
2024-12-26 13:28:52 +0100

    Update career history

--- src/components/Timeline.tsx
+++ src/components/Timeline.tsx
@@ -16,50 +16,52 @@ function Timeline() {
             className="vertical-timeline-element--work"
             contentStyle={{ background: 'white', color: 'rgb(39, 40, 34)' }}
             contentArrowStyle={{ borderRight: '7px solid  white' }}
-            date="2022 - present"
+            date="2023 - present"
             iconStyle={{ background: '#5000ca', color: 'rgb(39, 40, 34)' }}
             icon={<FontAwesomeIcon icon={faBriefcase} />}
           >
-            <h3 className="vertical-timeline-element-title">Technology Consultant</h3>
-            <h4 className="vertical-timeline-element-subtitle">Dallas, TX</h4>
+            <h3 className="vertical-timeline-element-title">Lead Backend Developer</h3>
+            <h4 className="vertical-timeline-element-subtitle">Enugu, Nigeria</h4>
             <p>
-              Full-stack Web Development, GenAI/LLM, Project Management, Business Development
+              Javascript, Typescript, Nest.js, MongoDB, RESTful APIs, Team Collaboration, Scrum, Agile, Git, GitHub, Postman, Swagger, Redis, Google Maps API, Telegram API
             </p>
           </VerticalTimelineElement>
           <VerticalTimelineElement
             className="vertical-timeline-element--work"
-            date="2020 - 2022"
+            date="2022 - 2023"
             iconStyle={{ background: '#5000ca', color: 'rgb(39, 40, 34)' }}
             icon={<FontAwesomeIcon icon={faBriefcase} />}
           >
-            <h3 className="vertical-timeline-element-title">Full Stack Engineer</h3>
-            <h4 className="vertical-timeline-element-subtitle">Laie, HI</h4>
+            <h3 className="vertical-timeline-element-title">Backend Development Intern</h3>
+            <h4 className="vertical-timeline-element-subtitle">Enugu, Nigeria</h4>
             <p>
-              Frontend Development, Backend Development, User Experience, Team Leading
+              Javascript, Typescript, Express.js, MongoDB, RESTful APIs, Team Collaboration, Scrum, Agile, Git, GitHub, Postman
             </p>
           </VerticalTimelineElement>
+
           <VerticalTimelineElement
             className="vertical-timeline-element--work"
-            date="2021 - 2021"
+            date="2021 - 2022"
             iconStyle={{ background: '#5000ca', color: 'rgb(39, 40, 34)' }}
             icon={<FontAwesomeIcon icon={faBriefcase} />}
           >
-            <h3 className="vertical-timeline-element-title">Staff Engineer Intern</h3>
-            <h4 className="vertical-timeline-element-subtitle">Laie, HI</h4>
+            <h3 className="vertical-timeline-element-title">IT Support Specialist</h3>
+            <h4 className="vertical-timeline-element-subtitle">Enugu, Nigeria</h4>
             <p>
-              Full-stack Development, API Development, User Experience
+             Troubleshooting, Networking, IT Support, Customer Service
             </p>
           </VerticalTimelineElement>
+
           <VerticalTimelineElement
             className="vertical-timeline-element--work"
-            date="2020 - 2020"
+            date="2018 - 2022"
             iconStyle={{ background: '#5000ca', color: 'rgb(39, 40, 34)' }}
             icon={<FontAwesomeIcon icon={faBriefcase} />}
           >
-            <h3 className="vertical-timeline-element-title">Data Analyst Intern</h3>
-            <h4 className="vertical-timeline-element-subtitle">Tokyo, Japan</h4>
+            <h3 className="vertical-timeline-element-title">Computer Engineering Undergraduate</h3>
+            <h4 className="vertical-timeline-element-subtitle">Enugu, Nigeria</h4>
             <p>
-              Automation, Data Governance, Statistical Analysis
+              Computer Engineering, Microcontrollers, Embedded Systems, Digital Electronics, Computer Networks, Software Engineering, Data Structures and Algorithms, Operating Systems, Computer Architecture
             </p>
           </VerticalTimelineElement>
         </VerticalTimeline>
